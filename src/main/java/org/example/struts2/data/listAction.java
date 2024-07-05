89d169263929fb548cddd0be266305f9a40e99e9
bart_zzb <bartzhaozb@gmail.com>
2024-07-05 21:20:34 +0800

    2024-07-05 Struts2 封装数据到list集合

---
+++ src/main/java/org/example/struts2/data/listAction.java
@@ -0,0 +1,30 @@
+package org.example.struts2.data;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.example.struts2.entity.Book;
+import org.example.struts2.entity.User;
+
+import java.util.List;
+
+/**
+ * 封装到list
+ */
+public class listAction extends ActionSupport {
+
+    private List<User> list;
+
+    public List<User> getList() {
+        return list;
+    }
+
+    public void setList(List<User> list) {
+        this.list = list;
+    }
+
+    @Override
+    public String execute() throws Exception {
+        System.out.println(list);
+        return NONE;
+    }
+
+}
